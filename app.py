d3fb7927bf65c12f6a2f73324fddd4b7e3e0163d
rithikayy <rithika1226@gmail.com>
2025-12-25 16:55:35 -0500

    agent into a function for frontend

--- app.py
+++ app.py
@@ -10,67 +10,66 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from langchain.agents import create_agent  
 
+def create_rag_agent():
+    load_dotenv()
+
+    if not os.environ.get("OPENAI_API_KEY"):
+        os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+    model = init_chat_model("gpt-4o-mini")
 
 
-load_dotenv()
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 
-if not os.environ.get("OPENAI_API_KEY"):
-    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+    vector_store = InMemoryVectorStore(embeddings)
 
-model = init_chat_model("gpt-4o-mini")
+    #STEP A: indexing
+    # step 1: loading the files from the folder
 
+    # notes for you: DocumentLoader is an abstract class, DirecLoader and TextLoader is implementation
+    loader = DirectoryLoader(
+        'gym_data/',
+        glob="**/*.txt",
+        loader_cls=TextLoader
+    ) # to do it per folder, use TextLoader !
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+    docs = loader.load()
 
-vector_store = InMemoryVectorStore(embeddings)
+    # step 2: splitting docs
 
-#STEP A: indexing
-# step 1: loading the files from the folder
+    # notes for you: TextSplitter is an abstract class
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True,)
+    all_splits = text_splitter.split_documents(docs)
+    print(f"Split blog post into {len(all_splits)} sub-documents.")
 
-# notes for you: DocumentLoader is an abstract class, DirecLoader and TextLoader is implementation
-loader = DirectoryLoader(
-    'gym_data/',
-    glob="**/*.txt",
-    loader_cls=TextLoader
-) # to do it per folder, use TextLoader !
+    # step 3: embeding and storing all these sub docs
+    doc_ids = vector_store.add_documents(documents=all_splits)
+    print(doc_ids[:3])
 
-docs = loader.load()
+    #STEP B: RAG ! relevant splits based on input is retrived (via Retrieval) and a model generates an answer 
+    # two types: agentic RAG, two-step chain
+    # agentic is when models can request a tool call (fetching data, searching web, etc) when answering a question
+    # two-step only required one inference call (agentic needs 2 for generating and producing final) but may no tbe as flexible
 
+    @dynamic_prompt
+    def prompt_w_context(request: ModelRequest):
+        # Adding context 
+        last_query = request.state["messages"][-1].text
+        retrieved_docs = vector_store.similarity_search(last_query) # get relevant docs
+        docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
 
-# step 2: splitting docs
+        system_msg = ("You are a helpful assistant. Use the following context in your response:" f"\n\n{docs_content}")
 
-# notes for you: TextSplitter is an abstract class
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, add_start_index=True,)
-all_splits = text_splitter.split_documents(docs)
-print(f"Split blog post into {len(all_splits)} sub-documents.")
+        return system_msg
 
-# step 3: embeding and storing all these sub docs
-doc_ids = vector_store.add_documents(documents=all_splits)
-print(doc_ids[:3])
+    agent = create_agent(model, tools=[], middleware=[prompt_w_context])
+    return agent
 
-
-#STEP B: RAG ! relevant splits based on input is retrived (via Retrieval) and a model generates an answer 
-# two types: agentic RAG, two-step chain
-# agentic is when models can request a tool call (fetching data, searching web, etc) when answering a question
-# two-step only required one inference call (agentic needs 2 for generating and producing final) but may no tbe as flexible
-
-@dynamic_prompt
-def prompt_w_context(request: ModelRequest):
-    # Adding context 
-    last_query = request.state["messages"][-1].text
-    retrieved_docs = vector_store.similarity_search(last_query) # get relevant docs
-    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
-
-    system_msg = ("You are a helpful assistant. Use the following context in your response:" f"\n\n{docs_content}")
-
-    return system_msg
-
-agent = create_agent(model, tools=[], middleware=[prompt_w_context])
-
-
-query = "What is a good workoout for begineers? i just started going to the gym"
-for step in agent.stream(
-    {"messages": [{"role": "user", "content": query}]},
-    stream_mode="values",
-):
-    step["messages"][-1].pretty_print()
+if __name__ == "__main__":
+    agent = create_rag_agent()
+    query = "What is a good workoout for begineers? i just started going to the gym"
+    for step in agent.stream(
+        {"messages": [{"role": "user", "content": query}]},
+        stream_mode="values",
+    ):
+        step["messages"][-1].pretty_print()
